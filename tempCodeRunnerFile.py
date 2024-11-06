05d9491c03d23ab94224700a16ec2a9c616526ef
Ariful Islam <ariful111866@gmail.com>
2024-11-06 22:48:58 +0600

    sampling error solved

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,10 @@
+fs = 6*sin_f
+# x_ = np.arange(st , stp , 1/fs)
+# y_sine = sine_amplitutde* np.sin(2 * np.pi * x_ * sin_f + (sine_phase *(np.pi/180)))
+# signal_plot_stem(x_ , y_sine , 3 , 'Up sampaling')
+
+# # Down sampling
+# fs = sin_f
+# x_ = np.arange(st , stp , 1/fs)
+# y_sine = sine_amplitutde* np.sin(2 * np.pi * x * sin_f + (sine_phase *(np.pi/180)))
+# signal_plot_s
